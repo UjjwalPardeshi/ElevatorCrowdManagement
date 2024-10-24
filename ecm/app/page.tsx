f4b14223d266657f6a91e2d425191f7497b4f35d
Kartikey Seth <codekartikey@gmail.com>
2024-10-24 19:06:46 +0530

    add navbar

--- ecm/app/page.tsx
+++ ecm/app/page.tsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from 'react';
 import { getDatabase, ref, onValue } from 'firebase/database';
 import app from './firebase/firebaseConfig';
+import Navbar from './components/navbar';
 const database = getDatabase(app);
 
 type LiftData = {
@@ -63,8 +64,8 @@ export default function Home() {
   };
 
   return (
-    <div className="min-h-screen p-4 flex flex-col items-center gap-5">
-      <h1 className="text-2xl font-bold mb-4 text-center">Elevator Crowd Management System</h1>
+    <div className="min-h-screen flex flex-col items-center gap-5 bg-gray-800">
+      <Navbar/>
 
       {Object.keys(lifts).map((liftKey) => {
         const lift = lifts[liftKey as keyof Lifts];
@@ -72,9 +73,9 @@ export default function Home() {
 
         return (
           <div key={liftKey} className={`mb-6 w-[70%] p-4 border-[3px] rounded-lg hover:scale-105 transition-all shadow-lg ${borderColor}`}>
-            <h2 className="text-xl font-semibold mb-2">{liftKey.replace('lift', 'Lift ')}</h2>
-            <div className="space-y-2">
-              <p><strong>Location:</strong> <span>{lift.location}</span></p>
+            <h2 className="text-xl font-semibold text-white mb-2">{liftKey.replace('lift', 'Lift ')}</h2>
+            <div className="space-y-2 text-white">
+              <p className='text-xl'><strong>Location:</strong> <span>{lift.location}</span></p>
               <p><strong>People Count:</strong> <span>{lift.people_count}</span></p>
               <p><strong>Crowd Density:</strong> <span>{lift.crowd_density}</span></p>
               <p><strong>Last Updated:</strong> <span>{lift.timestamp}</span></p>
