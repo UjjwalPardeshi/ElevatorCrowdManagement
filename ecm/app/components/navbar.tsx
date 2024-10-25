077628416761101683ee3808ba61351b93802db2
Kartikey Seth <codekartikey@gmail.com>
2024-10-25 07:41:15 +0530

    the final one

--- ecm/app/components/navbar.tsx
+++ ecm/app/components/navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
 
     return (
         <div className='bg-gray-950  w-full flex mb-10 items-center group justify-center gap-10  px-32 py-6'>
-            <Image onClick={handleReload} className=" scale-150  group-hover:scale-105 lg:scale-100  transition-all cursor-pointer" alt="logo" src="/logo.png" width={200} height={200}/>
+            <Image onClick={handleReload} className=" scale-50  group-hover:scale-105 lg:scale-100  transition-all cursor-pointer" alt="logo" src="/logo.png" width={200} height={200}/>
             <h1 onClick={handleReload} className="text-4xl self-center  hidden lg:block group-hover:scale-105 transition-all  font-bold text-center text-white cursor-pointer">
                 Elevator Crowd Management System
             </h1>
